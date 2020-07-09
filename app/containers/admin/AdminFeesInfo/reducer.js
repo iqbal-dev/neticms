67c5bcd402837fc401366d58bea2f580596c4883
Abdul Aziz Banna <aziz00747@yahoo.com>
2020-07-09 16:59:36 +0600

    fees info save and update in one form

--- app/containers/admin/AdminFeesInfo/reducer.js
+++ app/containers/admin/AdminFeesInfo/reducer.js
@@ -5,7 +5,8 @@
  */
 
 import { fromJS } from 'immutable';
-import { DEFAULT_ACTION, GET_FEES_INFO_LIST,SET_SAVE_ONCHANGE_FEE_NAME,GET_CLASS_INFO_LIST,GET_GROUP_INFO_LIST,SET_SAVE_ONCHANGE_SERIAL_NO,SET_SAVE_ONCHANGE_CLASS_LIST_VALUE,SET_SAVE_ONCHANGE_GROUP_LIST_VALUE, SET_SAVE_ONCHANGE_FEE_DETAILS,SET_SAVE_ONCHANGE_FEE_AMOUNT, SET_SAVE_ONCHANGE_FEE_TYPE, SAVE_FEE_INFO, UPDATE_FEE_INFO } from './constants';
+import { DEFAULT_ACTION, GET_FEES_INFO_LIST,SET_SAVE_ONCHANGE_FEE_PAYMENT_MODE,GET_DATATABLE_ROWDATA,SET_MODAL_STATUS,
+  SET_SAVE_ONCHANGE_FEE_NAME,GET_CLASS_INFO_LIST,GET_GROUP_INFO_LIST,SET_SAVE_ONCHANGE_SERIAL_NO,SET_SAVE_ONCHANGE_CLASS_LIST_VALUE,SET_SAVE_ONCHANGE_GROUP_LIST_VALUE, SET_SAVE_ONCHANGE_FEE_DETAILS,SET_SAVE_ONCHANGE_FEE_AMOUNT, SET_SAVE_ONCHANGE_FEE_TYPE, SAVE_FEE_INFO, UPDATE_FEE_INFO } from './constants';
 
 
 export const initialState = fromJS({
@@ -19,6 +20,10 @@ export const initialState = fromJS({
   feeType: '',
   classInfoList: [],
   groupInfoList: [],
+  paymentMode: '',
+  rowData: '',
+  modalVisible: false,
+
 
 });
 
@@ -45,8 +50,15 @@ function adminFeesInfoReducer(state = initialState, action) {
       return state.set('feeDetails', action.feeDetails);
     case SET_SAVE_ONCHANGE_FEE_AMOUNT:
       return state.set('feeAmount', action.feeAmount);
+    case SET_SAVE_ONCHANGE_FEE_PAYMENT_MODE:
+        return state.set('paymentMode', action.paymentMode);
+    case GET_DATATABLE_ROWDATA:
+          return state.set('rowData', action.rowData);
     case SET_SAVE_ONCHANGE_FEE_TYPE:
       return state.set('feeType', action.feeType);
+    case SET_MODAL_STATUS:
+        const status = state.get('modalVisible');
+        return state.set('modalVisible', !status);
 
     default:
       return state;
