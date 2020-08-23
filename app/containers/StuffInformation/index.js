6661fcf5523e46486b977fc11ae14c39b5944d56
Abdus Sobhan <sobhan3364@gmail.com>
2020-08-23 17:16:28 +0600

    Stuff Info image Updated

--- app/containers/StuffInformation/index.js
+++ app/containers/StuffInformation/index.js
@@ -20,7 +20,7 @@ import saga from './saga';
 import messages from './messages';
 import BreadcrumComponent from '../../components/BreadcrumComponent';
 
-import donorImage from '../../assets/img/donor-image.png';
+import donorImage from '../../assets/img/avatar.png';
 import { Button } from 'reactstrap';
 import { onSubmitStuffInfoSearchBtn, onSubmitStuffInfoSetRowData } from './actions';
 import { AppLayout } from '../AppLayout';
@@ -35,58 +35,58 @@ export class StuffInformation extends React.Component {
     return (
       <div>
         <AppLayout>
-        <BreadcrumComponent
-          pageTitle="Stuff's Information"
-          menuStepFirst="Institute Info"
-          menuStepSenond="List of"
-          menuStepThird="Stuff's Information"
-        />
-        <section>
-          <div className="container-fluid">
-            <div className="container p-t-60">
-              <div className="row">
+          <BreadcrumComponent
+            pageTitle="Stuff's Information"
+            menuStepFirst="Institute Info"
+            menuStepSenond="List of"
+            menuStepThird="Stuff's Information"
+          />
+          <section>
+            <div className="container-fluid">
+              <div className="container p-t-60">
+                <div className="row">
 
-                <div className="col-md-12">
-                  <div className="page-inner-title">
-                    <h2 className="text-orange">List of Stuff's</h2>
-                    <div className="custom-title-border-left" />
-                  </div>
-                </div>
-              </div>
-
-              <div className="row">
-                {this.props.staffInfoList.map( (item, index) =>
-                  <div className="col-md-4">
-                    <div className="grid-list-wrapper">
-                      <div className="grid-image">
-                        <img src={donorImage} className="mx-auto d-block" />
-                      </div>
-                      <div className="grid-content text-center">
-                        <div className="grid-title">
-                          <h3>{item.staffName}</h3>
-                        </div>
-                        <div className="grid-subtitle-title">
-                          <h4>{item.designationName}</h4>
-                        </div>
-                      </div>
-                      <div className="grid-social">
-                        <ul className="d-flex justify-content-center w-100 nav">
-                          <li><a className="phone" href="#" phoneNumber={item.staffMobile1}><i className="fas fa-phone" /></a></li>
-                          <li><a className="phone" href="#" phoneNumber={item.staffEmail}><i className="fas fa-envelope" /></a></li>
-                          <li><a href="#"><i className="fab fa-facebook-f" /></a></li>
-                          <li><a href="#"><i className="fab fa-linkedin-in" /></a></li>
-                        </ul>
-                      </div>
+                  <div className="col-md-12">
+                    <div className="page-inner-title">
+                      <h2 className="text-orange">List of Stuff's</h2>
+                      <div className="custom-title-border-left" />
                     </div>
                   </div>
-                )}
+                </div>
+
+                <div className="row">
+                  {this.props.staffInfoList.map((item, index) =>
+                    <div className="col-md-4">
+                      <div className="grid-list-wrapper">
+                        <div className="grid-image">
+                          {item.image ? <img src={"data:image/*;base64," + item.image} className="mx-auto d-block" /> : <img src={donorImage} className="mx-auto d-block" />}
+                        </div>
+                        <div className="grid-content text-center">
+                          <div className="grid-title">
+                            <h3>{item.staffName}</h3>
+                          </div>
+                          <div className="grid-subtitle-title">
+                            <h4>{item.designationName}</h4>
+                          </div>
+                        </div>
+                        <div className="grid-social">
+                          <ul className="d-flex justify-content-center w-100 nav">
+                            <li><a className="phone" href="#" phoneNumber={item.staffMobile1}><i className="fas fa-phone" /></a></li>
+                            <li><a className="phone" href="#" phoneNumber={item.staffEmail}><i className="fas fa-envelope" /></a></li>
+                            <li><a href="#"><i className="fab fa-facebook-f" /></a></li>
+                            <li><a href="#"><i className="fab fa-linkedin-in" /></a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-     
+          </section>
+
         </AppLayout>
-         </div>
+      </div>
     );
   }
 }
@@ -106,11 +106,11 @@ function mapDispatchToProps(dispatch) {
   return {
     dispatch,
     submitSearch: (evt) => {
-      console.log('dispatch',dispatch);
-      console.log('evt',evt);
+      console.log('dispatch', dispatch);
+      console.log('evt', evt);
       dispatch(onSubmitStuffInfoSetRowData('rowData')),
 
-      dispatch(onSubmitStuffInfoSearchBtn())
+        dispatch(onSubmitStuffInfoSearchBtn())
     },
 
   };
